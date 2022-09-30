040b49f3cdcb10566edb6b4059fce705897de2c5
admin <admin@ADMIN-PC>
2022-09-30 21:14:08 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+try
+{
+    int T = 2;
+double m = 0.2;
+Console.WriteLine("Введите A:");
+double A = double.Parse(Console.ReadLine());
+    double W = (2 * (Math.PI * Math.PI)) * (A * A) * m / (T * T);
+    Console.WriteLine(Math.Round(W, 2));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
